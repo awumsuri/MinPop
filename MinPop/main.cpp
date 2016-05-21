f1a885eee48c470b4565291f8173e7b33fdd4f53
Suri <awumsuri@gmail.com>
2016-05-21 19:03:48 -0400

    Initial Commit

---
+++ MinPop/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MinPop
+//
+//  Created by Suri on 5/21/16.
+//  Copyright © 2016 Suri. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
